e296f7c16cc43cb9cf8c4ebb80b5888bd22b11ca
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2023-10-12 22:28:45 +0600

    updated -- route

--- lib/data/repository/add_server_repo.dart
+++ lib/data/repository/add_server_repo.dart
@@ -1,36 +1,49 @@
-import 'package:dio/dio.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../../utils/app_constants.dart';
-import '../datasource/remote/dio/dio_client.dart';
-import '../datasource/remote/exception/api_error_handler.dart';
-import '../model/base_model/api_response.dart';
+import 'dart:convert';
 
-class AddServerRepo{
+import 'package:dio/dio.dart';
+import 'package:eye_vpn_lite_admin_panel/data/datasource/remote/dio/dio_client.dart';
+import 'package:eye_vpn_lite_admin_panel/data/model/base_model/api_response.dart';
+import 'package:eye_vpn_lite_admin_panel/utils/app_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../datasource/remote/exception/api_error_handler.dart';
+import 'package:dio/dio.dart' as dio;
+
+class AddServerRepo {
   final DioClient dioClient;
   final SharedPreferences sharedPreferences;
-  AddServerRepo({required this.dioClient,required this.sharedPreferences});
+  AddServerRepo({required this.dioClient, required this.sharedPreferences});
 
   /// For Add Server
-  Future<ApiResponse> addServer({dynamic country, dynamic username, dynamic password, dynamic config, dynamic image}) async {
+  Future<ApiResponse> addServer({
+    dynamic country,
+    dynamic username,
+    dynamic password,
+    dynamic config,
+    dynamic image,
+  }) async {
     try {
+      dio.FormData formData = dio.FormData.fromMap({
+        "country": country,
+        "username": username,
+        "password": password,
+        "config": config,
+        'image': image,
+      });
+
       dynamic response = await dioClient.post(
         AppConstants.addServerUrl,
-        data: {
-          "country": country,
-          "username": username,
-          "password": password,
-          "config": config,
-          "image": image,
-        },
+        data: formData,
         options: Options(headers: {
-          "Content-Type": "application/json",
+          // "Content-Type": "application/json",
+          'Content-Type': 'multipart/form-data',
           "Authorization": "Bearer ${sharedPreferences.getString(AppConstants.token) ?? ""}",
         }),
       );
+
       return ApiResponse.withSuccess(response);
     } catch (e) {
       return ApiResponse.withError(ApiErrorHandler.getMessage(e));
     }
   }
-
 }
--- lib/data/repository/server_update_repo.dart
+++ lib/data/repository/server_update_repo.dart
@@ -4,6 +4,7 @@ import '../../utils/app_constants.dart';
 import '../datasource/remote/dio/dio_client.dart';
 import '../datasource/remote/exception/api_error_handler.dart';
 import '../model/base_model/api_response.dart';
+import 'package:dio/dio.dart' as dio;
 
 class ServerUpdateRepo{
   final DioClient dioClient;
@@ -13,15 +14,18 @@ class ServerUpdateRepo{
   /// For Server Update
   Future<ApiResponse> serverUpdate({dynamic id, dynamic country, dynamic username, dynamic password, dynamic config, dynamic image}) async {
     try {
+
+      dio.FormData formData = dio.FormData.fromMap({
+        "country": country,
+        "username": username,
+        "password": password,
+        "config": config,
+        'image': image,
+      });
+
       dynamic response = await dioClient.post(
         AppConstants.serverUpdateUrl+id,
-        data: {
-          "country": country,
-          "username": username,
-          "password": password,
-          "config": config,
-          "image": image,
-        },
+        data: formData,
         options: Options(headers: {
           "Content-Type": "application/json",
           "Authorization": "Bearer ${sharedPreferences.getString(AppConstants.token) ?? ""}",
